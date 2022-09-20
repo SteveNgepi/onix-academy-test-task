bd5cdec3f944f0e9429a5761f8956ce8c16cb475
Steve <steve.ngepi@gmail.com>
2022-09-20 10:43:25 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const { response } = require('express');
+const express = require('express');
+const axios = require('axios').default;
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/rates' , (req, res) => {
+
+    const crypto = req.query.currency;
+
+    if(!crypto) {
+        res.status(404).send({message: "Enter cryptocurrency!"});
+    }
+
+    const config = {
+        method: 'get',
+        url: `http://api.coincap.io/v2/assets/${crypto}`,
+        headers: { }
+      };
+      
+      axios(config)
+      .then(function (response) {
+        const { priceUsd } = response.data.data;
+        res.send(`"usd": "${priceUsd}"`);
+      })
+      .catch(function (error) {
+        res.status(404).send({message: "Non-existent cryptocurrency!"});
+      });
+});
+
+
+app.listen(PORT, () => {
+  console.log("Сервер начал прослушивание запросов на порту 3000");
+});
