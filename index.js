d20d5f9c474178376f8d5cd9d0092493c4233b37
Steve <68343651+SteveNgepi@users.noreply.github.com>
2022-09-20 13:40:31 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
-const { response } = require('express');
-const express = require('express');
-const axios = require('axios').default;
+const { response } = require("express");
+const express = require("express");
+const axios = require("axios").default;
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.get('/rates' , (req, res) => {
+app.get("/rates", (req, res) => {
+  const crypto = req.query.currency;
 
-    const crypto = req.query.currency;
+  if (!crypto) {
+    res.status(404).send({ message: "Enter cryptocurrency!" });
+  }
 
-    if(!crypto) {
-        res.status(404).send({message: "Enter cryptocurrency!"});
-    }
+  const config = {
+    method: "get",
+    url: `http://api.coincap.io/v2/assets/${crypto}`,
+    headers: {},
+  };
 
-    const config = {
-        method: 'get',
-        url: `http://api.coincap.io/v2/assets/${crypto}`,
-        headers: { }
-      };
-      
-      axios(config)
-      .then(function (response) {
-        const { priceUsd } = response.data.data;
-        res.send(`"usd": "${priceUsd}"`);
-      })
-      .catch(function (error) {
-        res.status(404).send({message: "Non-existent cryptocurrency!"});
-      });
+  axios(config)
+    .then((response) => {
+      const { priceUsd } = response.data.data;
+      res.send(`"usd": "${priceUsd}"`);
+    })
+    .catch((error) => {
+      res.status(404).send({ message: "Non-existent cryptocurrency!" });
+    });
 });
 
-
 app.listen(PORT, () => {
-  console.log("Сервер начал прослушивание запросов на порту 3000");
+  console.log("Сервер начал прослушивание запросов на порту " + PORT);
 });
